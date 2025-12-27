65ded8c236720e72a9e2c92d5a5d191ba44074d1
lamberto <asghedom.lamberto@gmail.com>
2025-12-27 07:11:46 -0800

    authRoutes middleware ruling and added start script"

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const authRoutes = require('./routes/authRoutes')
 
+app.use('/auth', authRoutes)
 app.use(cors());
 app.use(express.json());
+
 
 const PORT = process.env.PORT || 3000;
 
