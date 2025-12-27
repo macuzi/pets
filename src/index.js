92d82b98eb7afffcea65e9d1105e384248ff1b22
lamberto <asghedom.lamberto@gmail.com>
2025-12-27 07:38:35 -0800

    (fix): swapped lines to ensure middleware reads req object

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const app = express();
 const authRoutes = require('./routes/authRoutes')
 
-app.use('/auth', authRoutes)
 app.use(cors());
 app.use(express.json());
+
+app.use('/auth', authRoutes)
 
 
 const PORT = process.env.PORT || 3000;
