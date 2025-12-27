140011468cc93b2b62622dcd4a31e513a9d7c02a
lamberto <asghedom.lamberto@gmail.com>
2025-12-27 15:22:25 -0800

    register pet routes in Express app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const petRoutes = require('./routes/petRoutes');
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
+app.use('/pets', petRoutes);
 
 
 const PORT = process.env.PORT || 3000;
